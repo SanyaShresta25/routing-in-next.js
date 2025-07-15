239cd2fae1493a8d9e9cb43cbe0512d7e81ba1f9
SanyaShresta25 <sanyashresta@gmail.com>
2025-07-15 22:31:09 +0530

    final

--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -1,7 +1,8 @@
 import { Metadata } from "next";
-
-
 import { courses } from "@/data/courses";
+import CourseCard from "@/components/CourseCard"; 
+import type { Course } from "@/types/course";
+
 export const metadata: Metadata = {
   title: "All Courses | LearnHub",
   description: "Browse over 100+ developer-friendly courses on LearnHub.",
@@ -14,9 +15,9 @@ export default function CoursePage() {
         All Courses ({courses.length})
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {/* {courses.map((course: Course) => (
+        {courses.map((course: Course) => (
           <CourseCard key={course.id} course={course} />
-        ))} */}
+        ))}
       </div>
     </main>
   );
