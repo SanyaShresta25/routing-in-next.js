5b07001fd868c73cb6814a5e87ed263acbe2582d
SanyaShresta25 <sanyashresta@gmail.com>
2025-07-15 22:14:11 +0530

    error

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,11 +3,10 @@
 export default function Loading() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-white text-purple-700">
-
       <div className="w-16 h-16 border-4 border-purple-500 border-t-transparent rounded-full animate-spin mb-6"></div>
 
       <h2 className="text-2xl font-semibold animate-pulse">
-        Just a moment, we're loading your content...
+        Just a moment, we&apos;re loading your content...
       </h2>
     </div>
   );
